c944698e63f8090dc1e12f829c6e16be23b5de57
Asya Lebedeva <star@asyaspace.com>
2025-11-05 22:49:48 +0700

    build: add functions for menu (open/close) and hidden extra code

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-/* console.log((`Approximately
-    1. Main 1280px: 14\n
-    2. Main 768px: 14\n
-    3. Main 320px: 0\n
-    4. Pets 1280px: 6\n
-    5. Pets 768px: 6\n
-    6. Pets 320px: 0\n
-    7. False,  0\n
-    8. False, 0\n
-    9. False, 0\n
-   10. 4\n
-   Sum: 44`
-   )) */
-
+/* draft slider. doesn't work now, I'll do it later.
+  
 const slides = document.querySelectorAll('.slide__list__item__card'),
     prev = document.querySelector('#prev'),
     next = document.querySelector('#next'),
@@ -28,7 +16,7 @@ slidesArea.style.width = 100 * slides.length + '%';
     slide.style.width = width;
 });*/
 
-next.addEventListener('click', () => {
+/*next.addEventListener('click', () => {
     if (offset == 0 || offset > 0) {
         offset = +width.slice(0, width.length - 2) * (slides.length - 1);
     } else {
@@ -45,4 +33,41 @@ prev.addEventListener('click', () => {
     }
 
     slidesArea.style.transform = `translateX(${+offset}px)`;
-})
+})*/
+const burgerBtn = document.querySelector('.burger');
+const nav = document.getElementById('menu');
+let axisY = 0;
+
+function stopBody() {
+   axisY = window.scrollY;
+   document.body.style.top = `-${axisY}px`;
+   document.body.classList.add('body-lock');
+}
+
+function moveBody() {
+    document.body.classList.remove('body-lock');
+    document.body.style.top = '';
+    window.scrollTo(0, axisY);
+}
+
+function openMenu() {
+    burgerBtn.classList.add('burger-active');
+    burgerBtn.setAttribute('aria-expanded', 'true');
+    nav.classList.add('open');
+    nav.setAttribute('aria-hidden', 'false');
+    stopBody();
+}
+
+function closeMenu() {
+    burgerBtn.classList.remove('burger-active');
+    burgerBtn.setAttribute('aria-expanded', 'false');
+    nav.classList.remove('open');
+    moveBody();
+}
+
+function toggleMenu() {
+    const isOpen = nav.classList.contains('open');
+    isOpen ? closeMenu() : openMenu();
+}
+
+burgerBtn.addEventListener('click', toggleMenu);
