696b26bafd5b9b4a8255eaa527dec9f5743217d2
Asya Lebedeva <star@asyaspace.com>
2026-02-08 21:48:31 +0700

    build: add components folder and script with card generation for slider and pagination

--- js/components/generateCard.js
+++ js/components/generateCard.js
@@ -3,9 +3,9 @@ const template = document.querySelector('.template');
 export function generatePetCard(pet) {
     const li = template.cloneNode(true);
     li.style.display = '';
-    const img = li.querySelector('.slider__list__item__card__pic__img');
+    const img = li.querySelector('.card__img');
     const source = li.querySelector('source');
-    const name = li.querySelector('.slider__list__item__card__name');
+    const name = li.querySelector('.card__name');
     img.src = pet.img;
     img.alt = pet.name;
     if(source) { source.srcset = pet.img};
