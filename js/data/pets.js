a21d4e0b4458492a677ec8a7ff93a1be8b95fdfb
Asya Lebedeva <star@asyaspace.com>
2026-02-16 21:26:53 +0700

    fix: update paths in array

--- js/data/pets.js
+++ js/data/pets.js
@@ -1,10 +1,10 @@
 export const pets = [
-    { name: "Winston", img: "../page/assets/img/pets-winston.png"},
-    { name: "Aldo", img: "../page/assets/img/pets-aldo.png"},
-    { name: "Donny", img: "../page/assets/img/pets-donny.png"},
-    { name: "Frederick", img: "../page/assets/img/pets-frederick.png"},
-    { name: "Emma", img: "../page/assets/img/pets-emma.png"},
-    { name: "Bridget", img: "../page/assets/img/pets-bridget.png"},
-    { name: "Hugo", img: "../page/assets/img/pets-hugo.png"},
-    { name: "Archie", img: "../page/assets/img/pets-archie.png"}
+    { name: "Winston", img: "/assets/img/pets-winston.png"},
+    { name: "Aldo", img: "/assets/img/pets-aldo.png"},
+    { name: "Donny", img: "/assets/img/pets-donny.png"},
+    { name: "Frederick", img: "/assets/img/pets-frederick.png"},
+    { name: "Emma", img: "/assets/img/pets-emma.png"},
+    { name: "Bridget", img: "/assets/img/pets-bridget.png"},
+    { name: "Hugo", img: "/assets/img/pets-hugo.png"},
+    { name: "Archie", img: "/assets/img/pets-archie.png"}
 ];
