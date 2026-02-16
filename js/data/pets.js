29e367d3404b50691c8a01e6973baf16d187aebd
Asya Lebedeva <star@asyaspace.com>
2026-02-16 23:16:18 +0700

    fix: rename object

--- js/data/pets.js
+++ js/data/pets.js
@@ -5,7 +5,7 @@ export const pets = [
     { name: "Aldo", img: `${BASE_PATH}img/pets-aldo.png`},
     { name: "Donny", img: `${BASE_PATH}img/pets-donny.png`},
     { name: "Frederick", img: `${BASE_PATH}img/pets-frederick.png`},
-    { name: "Emma", img: `${BASE_PATH}img/pets-emma.png`},
+    { name: "Shoshanna", img: `${BASE_PATH}img/pets-shoshanna.png`},
     { name: "Bridget", img: `${BASE_PATH}img/pets-bridget.png`},
     { name: "Hugo", img: `${BASE_PATH}img/pets-hugo.png`},
     { name: "Archie", img: `${BASE_PATH}img/pets-archie.png`}
